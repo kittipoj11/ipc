4a91f84cda7555b86e36738074923630b636f5dd
kittipoj11 <poj11@hotmail.com>
2024-12-25 16:06:59 +0700

    Update ipc database at 25/12/2024

--- database/inspection_db.sql
+++ database/inspection_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 06, 2024 at 10:12 AM
+-- Generation Time: Dec 24, 2024 at 08:50 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.1.17
 
@@ -24,63 +24,129 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `inspect_detail`
+-- Table structure for table `approval_levels`
 --
 
-CREATE TABLE `inspect_detail` (
-  `inspect_no` varchar(50) NOT NULL,
-  `po_no` varchar(50) NOT NULL,
-  `location_code` int(11) DEFAULT NULL,
-  `working_name` varchar(255) DEFAULT NULL,
-  `working_date_from` date DEFAULT NULL,
-  `working_date_to` date DEFAULT NULL,
-  `working_day` smallint(6) DEFAULT NULL,
-  `period` tinyint(4) NOT NULL,
-  `is_deposit` tinyint(1) DEFAULT NULL,
-  `deposit` decimal(10,2) DEFAULT NULL,
-  `net_value_current_claim` decimal(10,2) DEFAULT NULL,
-  `less_previous_interim_payment` decimal(10,2) DEFAULT NULL,
-  `total_value_interim_payment` decimal(10,2) DEFAULT NULL,
-  `remain_value_interim_payment` decimal(10,2) DEFAULT NULL,
-  `total_value_retension` decimal(10,2) DEFAULT NULL,
-  `completed_work_volume_plan` decimal(10,2) DEFAULT NULL,
-  `completed_work_volume_current` decimal(10,2) DEFAULT NULL,
-  `completed_work_volume_remain` decimal(10,2) DEFAULT NULL,
-  `status_plan` varchar(1) DEFAULT NULL,
-  `is_paid` tinyint(1) DEFAULT NULL,
-  `is_retention` tinyint(1) DEFAULT NULL,
-  `remark` varchar(255) DEFAULT NULL,
-  `list_json` longtext DEFAULT NULL,
-  `create_by` varchar(50) DEFAULT NULL,
-  `create_date` date DEFAULT NULL,
-  `inspector` varchar(50) DEFAULT NULL,
-  `inspect_date` date DEFAULT NULL,
-  `approver1` varchar(50) DEFAULT NULL,
-  `approve_date1` date DEFAULT NULL,
-  `approver2` varchar(50) DEFAULT NULL,
-  `approve_date2` date DEFAULT NULL,
-  `approver3` varchar(50) DEFAULT NULL,
-  `approve_date3` date DEFAULT NULL,
-  `approver4` varchar(50) DEFAULT NULL,
-  `approve_date4` date DEFAULT NULL,
-  `approver5` varchar(50) DEFAULT NULL,
-  `approve_date5` date DEFAULT NULL
+CREATE TABLE `approval_levels` (
+  `level_id` int(11) NOT NULL,
+  `workflow_id` int(11) DEFAULT NULL,
+  `level_order` int(11) DEFAULT NULL,
+  `approver_role` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `inspect_header`
+-- Table structure for table `approval_status`
 --
 
-CREATE TABLE `inspect_header` (
-  `inspect_no` varchar(50) NOT NULL,
-  `po_no` varchar(50) NOT NULL,
-  `contract_value` decimal(10,2) DEFAULT NULL,
-  `remain_value_interim_payment` decimal(10,2) DEFAULT NULL,
-  `total_value_retension` decimal(10,2) DEFAULT NULL,
-  `create_by` varchar(50) DEFAULT NULL,
-  `create_date` date DEFAULT NULL
+CREATE TABLE `approval_status` (
+  `approval_status_id` int(11) NOT NULL,
+  `approval_status_name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `approval_workflow`
+--
+
+CREATE TABLE `approval_workflow` (
+  `workflow_id` int(11) NOT NULL,
+  `workflow_name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `departments`
+--
+
+CREATE TABLE `departments` (
+  `department_id` int(11) NOT NULL,
+  `department_name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `inspect_approvals`
+--
+
+CREATE TABLE `inspect_approvals` (
+  `approval_id` int(11) NOT NULL,
+  `inspect_id` int(11) DEFAULT NULL,
+  `period` int(11) DEFAULT NULL,
+  `level_id` int(11) DEFAULT NULL,
+  `approver_id` int(11) DEFAULT NULL,
+  `approval_status_id` int(11) DEFAULT NULL,
+  `approval_date` datetime DEFAULT current_timestamp(),
+  `comments` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `inspect_main`
+--
+
+CREATE TABLE `inspect_main` (
+  `inspect_id` int(11) NOT NULL,
+  `po_id` int(11) DEFAULT NULL,
+  `working_date_from` date DEFAULT NULL,
+  `working_date_to` date DEFAULT NULL,
+  `working_day` int(11) DEFAULT NULL,
+  `remain_value_interim_payment` decimal(15,2) DEFAULT NULL,
+  `total_retention_value` decimal(15,2) DEFAULT NULL,
+  `inspect_status` varchar(50) DEFAULT NULL,
+  `create_by` int(11) DEFAULT NULL,
+  `create_date` datetime DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `inspect_period`
+--
+
+CREATE TABLE `inspect_period` (
+  `inspect_period_id` int(11) NOT NULL,
+  `inspect_id` int(11) DEFAULT NULL,
+  `period` int(11) DEFAULT NULL,
+  `workload_planned_percent` decimal(5,2) DEFAULT NULL,
+  `workload_actual_completed_percent` decimal(5,2) DEFAULT NULL,
+  `workload_remaining_percent` decimal(5,2) DEFAULT NULL,
+  `interim_payment` decimal(15,2) DEFAULT NULL,
+  `interim_payment_percent` decimal(5,2) DEFAULT NULL,
+  `interim_payment_less_previous` decimal(15,2) DEFAULT NULL,
+  `interim_payment_less_previous_percent` decimal(5,2) DEFAULT NULL,
+  `interim_payment_accumulated` decimal(15,2) DEFAULT NULL,
+  `interim_payment_accumulated_percent` decimal(5,2) DEFAULT NULL,
+  `interim_payment_remain` decimal(15,2) DEFAULT NULL,
+  `interim_payment_remain_percent` decimal(5,2) DEFAULT NULL,
+  `retention_value` decimal(15,2) DEFAULT NULL,
+  `plan_status` varchar(50) DEFAULT NULL,
+  `is_paid` tinyint(1) DEFAULT 0,
+  `is_retention` tinyint(1) DEFAULT 0,
+  `remark` varchar(255) DEFAULT NULL,
+  `workflow_id` int(11) DEFAULT NULL,
+  `current_status` varchar(50) DEFAULT NULL,
+  `current_level` int(11) DEFAULT NULL,
+  `list_json` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL CHECK (json_valid(`list_json`))
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `inspect_period_detail`
+--
+
+CREATE TABLE `inspect_period_detail` (
+  `inspect_period_detail_id` int(11) NOT NULL,
+  `inspect_period_id` int(11) DEFAULT NULL,
+  `order_no` int(11) DEFAULT NULL,
+  `details` varchar(255) DEFAULT NULL,
+  `remark` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 -- --------------------------------------------------------
@@ -90,7 +156,7 @@ CREATE TABLE `inspect_header` (
 --
 
 CREATE TABLE `location` (
-  `location_code` int(11) NOT NULL,
+  `location_id` int(11) NOT NULL,
   `location_name` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
@@ -101,14 +167,33 @@ CREATE TABLE `location` (
 --
 
 CREATE TABLE `po` (
+  `po_id` int(11) NOT NULL,
   `po_no` varchar(50) NOT NULL,
-  `project_name` varchar(255) DEFAULT NULL,
-  `suppliers_code` int(11) DEFAULT NULL,
-  `contract_value` decimal(10,2) DEFAULT NULL,
-  `retention_percent` decimal(4,2) DEFAULT NULL,
-  `retention_value` decimal(10,2) DEFAULT 5.00,
-  `create_by` varchar(50) DEFAULT NULL,
-  `create_date` date DEFAULT NULL
+  `project_id` int(11) DEFAULT NULL,
+  `suppliers_id` int(11) DEFAULT NULL,
+  `location_id` int(11) DEFAULT NULL,
+  `working_name_th` varchar(255) DEFAULT NULL,
+  `working_name_en` varchar(255) DEFAULT NULL,
+  `is_include_vat` tinyint(1) DEFAULT 1,
+  `contract_value` decimal(15,2) DEFAULT NULL,
+  `contract_value_before` decimal(15,2) DEFAULT NULL,
+  `vat` decimal(15,2) DEFAULT NULL,
+  `is_deposit` tinyint(1) DEFAULT 0,
+  `deposit_percent` decimal(5,2) DEFAULT NULL,
+  `deposit_value` decimal(15,2) DEFAULT NULL,
+  `create_by` int(11) DEFAULT NULL,
+  `create_date` datetime DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `project`
+--
+
+CREATE TABLE `project` (
+  `project_id` int(11) NOT NULL,
+  `project_name` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 -- --------------------------------------------------------
@@ -118,23 +203,23 @@ CREATE TABLE `po` (
 --
 
 CREATE TABLE `suppliers` (
-  `suppliers_code` int(11) NOT NULL,
+  `suppliers_id` int(11) NOT NULL,
   `supplier_name` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `user`
+-- Table structure for table `users`
 --
 
-CREATE TABLE `user` (
-  `user_name` varchar(50) NOT NULL,
-  `password` varchar(255) NOT NULL DEFAULT '''''',
-  `firstname` varchar(255) DEFAULT NULL,
-  `lastname` varchar(255) DEFAULT NULL,
-  `position` varchar(50) DEFAULT NULL,
-  `department_code` varchar(20) DEFAULT NULL
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `full_name` varchar(255) DEFAULT NULL,
+  `role` varchar(50) DEFAULT NULL,
+  `department_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
@@ -142,56 +227,166 @@ CREATE TABLE `user` (
 --
 
 --
--- Indexes for table `inspect_detail`
+-- Indexes for table `approval_levels`
 --
-ALTER TABLE `inspect_detail`
-  ADD PRIMARY KEY (`po_no`,`period`);
+ALTER TABLE `approval_levels`
+  ADD PRIMARY KEY (`level_id`);
 
 --
--- Indexes for table `inspect_header`
+-- Indexes for table `approval_status`
 --
-ALTER TABLE `inspect_header`
-  ADD PRIMARY KEY (`po_no`);
+ALTER TABLE `approval_status`
+  ADD PRIMARY KEY (`approval_status_id`);
+
+--
+-- Indexes for table `approval_workflow`
+--
+ALTER TABLE `approval_workflow`
+  ADD PRIMARY KEY (`workflow_id`);
+
+--
+-- Indexes for table `departments`
+--
+ALTER TABLE `departments`
+  ADD PRIMARY KEY (`department_id`);
+
+--
+-- Indexes for table `inspect_approvals`
+--
+ALTER TABLE `inspect_approvals`
+  ADD PRIMARY KEY (`approval_id`);
+
+--
+-- Indexes for table `inspect_main`
+--
+ALTER TABLE `inspect_main`
+  ADD PRIMARY KEY (`inspect_id`);
+
+--
+-- Indexes for table `inspect_period`
+--
+ALTER TABLE `inspect_period`
+  ADD PRIMARY KEY (`inspect_period_id`);
+
+--
+-- Indexes for table `inspect_period_detail`
+--
+ALTER TABLE `inspect_period_detail`
+  ADD PRIMARY KEY (`inspect_period_detail_id`);
 
 --
 -- Indexes for table `location`
 --
 ALTER TABLE `location`
-  ADD PRIMARY KEY (`location_code`);
+  ADD PRIMARY KEY (`location_id`);
 
 --
 -- Indexes for table `po`
 --
 ALTER TABLE `po`
-  ADD PRIMARY KEY (`po_no`);
+  ADD PRIMARY KEY (`po_id`),
+  ADD UNIQUE KEY `po_no` (`po_no`);
+
+--
+-- Indexes for table `project`
+--
+ALTER TABLE `project`
+  ADD PRIMARY KEY (`project_id`);
 
 --
 -- Indexes for table `suppliers`
 --
 ALTER TABLE `suppliers`
-  ADD PRIMARY KEY (`suppliers_code`);
+  ADD PRIMARY KEY (`suppliers_id`);
 
 --
--- Indexes for table `user`
+-- Indexes for table `users`
 --
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`user_name`);
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`),
+  ADD UNIQUE KEY `username` (`username`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
+-- AUTO_INCREMENT for table `approval_levels`
+--
+ALTER TABLE `approval_levels`
+  MODIFY `level_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `approval_status`
+--
+ALTER TABLE `approval_status`
+  MODIFY `approval_status_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `approval_workflow`
+--
+ALTER TABLE `approval_workflow`
+  MODIFY `workflow_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `departments`
+--
+ALTER TABLE `departments`
+  MODIFY `department_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `inspect_approvals`
+--
+ALTER TABLE `inspect_approvals`
+  MODIFY `approval_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `inspect_main`
+--
+ALTER TABLE `inspect_main`
+  MODIFY `inspect_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `inspect_period`
+--
+ALTER TABLE `inspect_period`
+  MODIFY `inspect_period_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `inspect_period_detail`
+--
+ALTER TABLE `inspect_period_detail`
+  MODIFY `inspect_period_detail_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `location`
 --
 ALTER TABLE `location`
-  MODIFY `location_code` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `location_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `po`
+--
+ALTER TABLE `po`
+  MODIFY `po_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `project`
+--
+ALTER TABLE `project`
+  MODIFY `project_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `suppliers`
 --
 ALTER TABLE `suppliers`
-  MODIFY `suppliers_code` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `suppliers_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
