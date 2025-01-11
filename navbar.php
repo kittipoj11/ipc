be262252e5fd66a7e7ebe820069db65e3654183b
kittipoj11 <poj11@hotmail.com>
2025-01-11 22:56:53 +0700

    Add undraw_profile.svg

--- navbar.php
+++ navbar.php
@@ -19,9 +19,9 @@
         <!-- Nav Item - User Information -->
         <li class="nav-item dropdown no-arrow">
             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <!-- <span class="mr-2 d-none d-lg-inline text-gray-600 small">< ?php echo $_SESSION['fullname'] ?></span> -->
-                <span class="mr-2 d-none d-lg-inline text-gray-600 small"><?php echo $_SESSION['fullname'] ?></span>
-                <img class="rounded-circle" style="height: 2rem;width: 2rem;" src="../dist/img/undraw_profile.svg">
+                <!-- <span class="mr-2 d-none d-lg-inline text-gray-600 small">< ?php echo $_SESSION['full_name'] ?></span> -->
+                <span class="mr-2 d-none d-lg-inline text-gray-600 small"><?php echo $_SESSION['full_name'] ?></span>
+                <img class="rounded-circle" style="height: 2rem;width: 2rem;" src="images/undraw_profile.svg">
             </a>
             <!-- Dropdown - User Information -->
             <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
--- sidebar.php
+++ sidebar.php
@@ -3,7 +3,7 @@
     <!-- Brand Logo -->
     <a href="index.php" class="brand-link">
         <img src="images/mypic.jpg" alt="Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-        <span class="brand-text font-weight-light">Car Staging</span>
+        <span class="brand-text font-weight-light">IPC</span>
     </a>
 
 
